e98b295c70054b5822ac15b4b055fc1e6794d69c
Aleksi95 <aleksi.laiho@helsinki.fi>
2024-12-03 14:18:07 +0200

    Update Metrics.R

--- R/Metrics.R
+++ R/Metrics.R
@@ -627,6 +627,36 @@ gPCA_percentage = function(data, sample_types, nperm = 1000){
 }
 
 
+kldist <-
+  function(xba, batch) {
+    
+    xba <- scale(xba)
+    
+    allpairs <- combn(length(levels(batch)), 2)
+    
+    sum(apply(allpairs, 2, function(y) (sum(batch==y[1]) + sum(batch==y[2]))*kldistTwo(xba[batch==y[1],], xba[batch==y[2],]))/(nrow(xba)*(length(levels(batch))-1)))
+    
+  }
+
+kldistTwo <-
+  function(xb1, xb2) {
+    
+    ##require("FNN")
+    
+    distmat1 <- as.matrix(dist(xb1))
+    dists1 <- distmat1[lower.tri(distmat1)]
+    
+    distmat2 <- as.matrix(dist(xb2))
+    dists2 <- distmat2[lower.tri(distmat2)]
+    
+    distmat12 <- apply(xb1, 1, function(y) sqrt(rowSums(sweep(xb2, 2, y, "-")^2)))
+    dists12 <- as.vector(distmat12)
+    
+    (nrow(xb1)*FNN::KL.dist(dists1, dists12, k=5)[5] + nrow(xb2)*
+        FNN::KL.dist(dists2, dists12, k=5)[5])/(nrow(xb1) + nrow(xb2))
+    
+  }
+
 
 
 
